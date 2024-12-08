4846b8e1a6f61d50861b2672f20797d522500a43
Kooreis <kooreis.official@gmail.com>
2024-12-08 16:01:51 -0600

    Imports necessary libraries and declares the EventEmitter class with a private Map to hold listeners.

--- solution.java
+++ solution.java
@@ -1,4 +1,3 @@
-```java
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -7,42 +6,4 @@ import java.util.function.Consumer;
 
 public class EventEmitter {
     private Map<String, List<Consumer<Object>>> listeners = new HashMap<>();
-
-    public void subscribe(String event, Consumer<Object> listener) {
-        if (!listeners.containsKey(event)) {
-            listeners.put(event, new ArrayList<>());
-        }
-        listeners.get(event).add(listener);
-    }
-
-    public void unsubscribe(String event, Consumer<Object> listener) {
-        if (listeners.containsKey(event)) {
-            listeners.get(event).remove(listener);
-        }
-    }
-
-    public void emit(String event, Object data) {
-        if (listeners.containsKey(event)) {
-            for (Consumer<Object> listener : listeners.get(event)) {
-                listener.accept(data);
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        EventEmitter emitter = new EventEmitter();
-        Consumer<Object> listener1 = data -> System.out.println("Listener 1 received: " + data);
-        Consumer<Object> listener2 = data -> System.out.println("Listener 2 received: " + data);
-
-        emitter.subscribe("event1", listener1);
-        emitter.subscribe("event1", listener2);
-        emitter.subscribe("event2", listener1);
-
-        emitter.emit("event1", "Hello");
-        emitter.emit("event2", "World");
-
-        emitter.unsubscribe("event1", listener1);
-        emitter.emit("event1", "Hello again");
-    }
 }
-```
