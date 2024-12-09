24f170da9a98c7b17dda9c227e2f8261fe09dc54
Kooreis <kooreis.official@gmail.com>
2024-12-09 16:01:51 -0600

    Adds the 'subscribe' method to the EventEmitter class, which allows listeners to subscribe to an event.

--- solution.java
+++ solution.java
@@ -1,9 +1,6 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.function.Consumer;
-
-public class EventEmitter {
-    private Map<String, List<Consumer<Object>>> listeners = new HashMap<>();
-}
+public void subscribe(String event, Consumer<Object> listener) {
+        if (!listeners.containsKey(event)) {
+            listeners.put(event, new ArrayList<>());
+        }
+        listeners.get(event).add(listener);
+    }
