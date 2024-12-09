cd3e73fa9169a781f287d93fc015c7e9075aaf48
Kooreis <kooreis.official@gmail.com>
2024-12-09 16:01:51 -0600

    This update adds the Subscribe and Unsubscribe methods to the EventEmitter class. The Subscribe method adds a handler to an event, and the Unsubscribe method removes a handler from an event.

--- solution.cs
+++ solution.cs
@@ -1,9 +1,18 @@
 ```csharp
-using System;
-using System.Collections.Generic;
-
-public class EventEmitter
+public void Subscribe(string eventName, Action handler)
 {
-    private Dictionary<string, Action> events = new Dictionary<string, Action>();
+    if (!events.ContainsKey(eventName))
+    {
+        events[eventName] = null;
+    }
+    events[eventName] += handler;
+}
+
+public void Unsubscribe(string eventName, Action handler)
+{
+    if (events.ContainsKey(eventName))
+    {
+        events[eventName] -= handler;
+    }
 }
 ```
